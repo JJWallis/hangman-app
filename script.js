6f69e5487d726a1b61ad0223884f750740dbfd44
Joshua Jameson-Wallis <jjw@Joshuas-MacBook-Air.local>
2021-07-28 20:56:28 +0100

    Styles + endgame function

---
+++ script.js
@@ -0,0 +1,44 @@
+import hangmanImages from './data.js'
+const words = ['able','acid','aged','also','area','army','away','baby','band','bank']
+const wrapper = document.querySelector('#container')
+const title = document.querySelector('#main-title')
+const hangman = document.querySelector('#hangman-img')
+let triesRemaining = document.querySelector('#tries-remaining')
+const guess = document.querySelector('#usrs-input')
+const btnCheck = document.querySelector('#button-check')
+let wordToGuess = document.querySelector('#word-to-guess')
+let counter = 0
+let guessesLeft = 6
+let isPlaying = false
+
+
+const functions = {
+    element: (el, prop, val) => el[prop] = val
+}
+
+if (isPlaying) {
+
+} else {
+    endGame()
+}
+
+
+// ======================= END GAME ========================
+
+function btnReset () {
+    const btn = document.createElement('button')
+    functions.element(btn, 'type', 'button')
+    functions.element(btn, 'textContent', 'Reset')
+    functions.element(btn, 'className', 'reset')
+    return btn
+}
+
+function endGame () {
+    const btn = btnReset()
+    wrapper.append(btn)
+    functions.element(title, 'innerText', 'Click the reset button bellow to play again!')
+    wordToGuess = '____'
+    counter = 0
+    guessesLeft = 6
+    functions.element(triesRemaining, 'innerText', guessesLeft)
+}
